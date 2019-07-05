7b35ce2db186f2226e029ffcc453a8c505bf2be7
ricardo_ruiz <rfpimpao@gmail.com>
2019-07-05 02:06:20 +0100

    added routes to placeholder REST Api

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
 import About from './components/pages/About';
-import uuid from 'uuid';
+// import uuid from 'uuid';
+import axios from 'axios';
 import './App.css';
 
 
@@ -22,39 +23,34 @@ class App extends Component {
 
   // Delete Todo
   delTodo = (id) => {
-    this.setState({ todos: [...this.state.todos.filter((todo) => 
-      todo.id!==id
-      )] });
+    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`).then((res) => {
+      this.setState({ todos: [...this.state.todos.filter((todo) => 
+        todo.id!==id
+        )] });
+    })
+    
   }
 
   // Add Todo
   addTodo = (title) => {
-    const newTodo = {
-      id: uuid.v4(),
-      title: title,
+    axios.post('https://jsonplaceholder.typicode.com/todos', {
+      title,
       completed: false
-    }
-    this.setState({ todos: [...this.state.todos, newTodo] })
+    }).then((res) => {
+      this.setState({ todos: [...this.state.todos, res.data] })
+    })
+    
   }
 
   state = {
-    todos: [
-      {
-        id: uuid.v4(),
-        title: 'Take out the trash',
-        completed: false
-      },
-      {
-        id: uuid.v4(),
-        title: 'Dinner with wife',
-        completed: false
-      },
-      {
-        id: uuid.v4(),
-        title: 'Meeting with boss',
-        completed: false
-      }
-    ]
+    todos: []
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    .then((res) => {
+      this.setState({ todos: res.data });
+    });
   }
 
   render () {
